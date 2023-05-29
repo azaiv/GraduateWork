a56642f79b329a936e1a2038f69b07bebdb114b3
azaiv <azkrv26@gmail.com>
2023-05-29 17:51:38 +0300

    add icon to app

---
+++ SpaceCraft/Module/Controller/PageViewController/PageViewController + DataSource.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        rocketViewController.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return [rocketViewController].startIndex
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = rocketViewController.firstIndex(of: viewController as! TestViewController) else { return nil }
+        
+        if index == 0 {
+            return rocketViewController.last
+        } else {
+            
+            return rocketViewController[index - 1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = rocketViewController.firstIndex(of: viewController as! TestViewController) else { return nil }
+        if viewController == rocketViewController.last {
+            return rocketViewController.first
+        } else {
+            return rocketViewController[index + 1]
+        }
+    }
+}
+
